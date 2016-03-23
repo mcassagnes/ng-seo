d9e7161864a1b3f26360728ee29f95be5595e538
Mathieu Cassagnes <mcassagnes@articho.co>
2016-03-22 21:08:55 -0400

    First commit

---
+++ ngSeo.js
@@ -0,0 +1,54 @@
+(function() {
+  "use strict";
+
+  angular.module("ngSeo", [])
+
+  .run(["$rootScope", "ngSeo", function($rootScope, ngSeo) {
+    ngSeo.init();
+    $rootScope.ngSeo = ngSeo;
+  }])
+
+  .service("ngSeo", [function() {
+    var self = this;
+
+    // Default seo tags
+    // These are the most commonly used
+    var tags = {
+      "title": null,
+      "description": null,
+      "robots": null,
+      "og:url": null,
+      "og:type": null,
+      "og:title": null,
+      "og:description": null,
+      "og:image": null
+    };
+
+    this.get = function(key) {
+      return tags[key];
+    };
+
+    this.set = function(key, val) {
+      tags[key] = val;
+    };
+
+    this.init = function(opts) {
+      self.reset();
+      if (opts && typeof opts === "object") {
+        for (var key in opts) {
+          if (opts.hasOwnProperty(key)) {
+            tags[key] = opts[key];
+          }
+        }
+      }
+    };
+
+    this.reset = function() {
+      for (var key in opts) {
+        if (opts.hasOwnProperty(key)) {
+          tags[key] = null;
+        }
+      }
+    };
+  }]);
+})();
